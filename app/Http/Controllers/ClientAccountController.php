2c39b2318b42892c0f01e84e0d22ad30d3c67194
Hassan Raza <ranahasanraza24@gmail.com>
2022-10-05 14:58:15 +0500

    auth added at client account

--- app/Http/Controllers/ClientAccountController.php
+++ app/Http/Controllers/ClientAccountController.php
@@ -13,6 +13,13 @@ class ClientAccountController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct(){
+
+        $this->middleware('auth');
+        
+    }
+
     public function index()
     {
         $accounts = ClientAccount::where('user_id', Auth::user()->id);
